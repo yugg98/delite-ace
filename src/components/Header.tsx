b373aa7c4a2e0489ddf457a20f6e8be89cc7be1b
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-11-11 18:07:25 +0530

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,7 @@ const Header = () => {
           open={mobileMenuOpen}
           onClose={setMobileMenuOpen}
         >
-          <div className="fixed inset-0 z-50" />
+          <div className="fixed inset-0 z-[9999px]" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#0A1022] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <Link href="/" className="-m-1.5 p-1.5">
