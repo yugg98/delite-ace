82a893dc9c7bd4503f22e8c228a08db586f9e201
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-11-11 17:53:31 +0530

    done

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,161 +1,157 @@
-import React from "react";
+const featuredTestimonial = {
+  body: "Deliteace's digital signage was a game-changer for our business—intuitive software, outstanding clarity, and robust design. Customer engagement skyrocketed, and so did our profits. Absolutely the best investment!",
+  author: {
+    name: 'Yug Gupta',
+    handle: 'https://startshorts.com/',
+    imageUrl:
+      'https://www.startshorts.com/_next/static/media/startshorts.c99167e3.svg',
+  },
+}
+const testimonials = [
+  [
+    [
+      {
+        body: "Since installing Deliteace's digital signs, our sales have soared. Vibrant displays and easy updates have made all the difference. Highly recommend",
+        author: {
+          name: 'Fortishealthcare',
+          handle: 'www.fortishealthcare.com',
+          imageUrl:
+            'https://www.fortishealthcare.com/themes/custom/fortis/logo.png',
+        },
+      },
+      // More testimonials...
+    ],
+    [
+      {
+        body: "Deliteace's digital signage transformed our lobby. Sleek design, engaging content—guests are impressed, and our message is clear!",
+        author: {
+          name: 'Organic India',
+          handle: 'https://organicindia.com/',
+          imageUrl:
+            'https://wilyer.com/assets/retail/organic.svg',
+        },
+      },
+      // More testimonials...
+    ],
+  ],
+  [
+    [
+      {
+        body: "Incredible ROI with Deliteace's signs. Easy content management, vibrant screens. They've elevated our advertising to new heights!",
+        author: {
+          name: 'Hyatt',
+          handle: 'https://www.hyatt.com/',
+          imageUrl:
+            'https://wilyer.com/assets/hospitality/hyatt.svg',
+        },
+      },
+      // More testimonials...
+    ],
+    [
+      {
+        body: 'Thanks to Deliteace, our menus shine! Crisp visuals, effortless updates—customers love it, and our sales reflect that!',
+        author: {
+          name: 'Canon',
+          handle: 'https://in.canon/',
+          imageUrl:
+            'https://wilyer.com/assets/retail/canon.svg',
+        },
+      },
+      // More testimonials...
+    ],
+  ],
+]
 
-const Testimonials = () => {
+function classNames(...classes:any) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
   return (
-    <div id="testimonials">
-      <section>
-        <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
-          <div className="mx-auto mb-8 max-w-3xl text-center md:mb-12 lg:mb-16">
-            <h1 className="text-4xl font-semibold  tracking-tight text-white sm:text-5xl">
-              What Our Client&apos;s say
-            </h1>
-          <div className="gr-1 left-0 absolute max-w-full" />
-            <p className="mt-6  max-w-2xl mx-auto  text-gray-200">
-              Transform your space with our state-of-the-art digital signage
-              solutions. Elevate communication, captivate audiences, and deliver
-              messages with impact.
-            </p>
-          </div>   
-          <div className="gr-1  max-w-full absolute" />
-          <div className="grid grid-cols-1 gap-8 sm:gap-20 lg:grid-cols-2">
-            <img
-              src="https://uploads-ssl.webflow.com/6458c625291a94a195e6cf3a/6458c625291a94090ae6cf95_Rectangle%2035.png"
-              alt=""
-              className="sm:hidden"
-            />
-            <div className="hidden h-full w-full grid-cols-5 gap-5 sm:grid">
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28e7_Testimonial%20Standard%20Card.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28e5_Testimonial%20Standard%20Card-18.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28e3_Testimonial%20Standard%20Card-19.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28e1_Testimonial%20Standard%20Card-17.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28d9_Testimonial%20Standard%20Card-15.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28df_Testimonial%20Standard%20Card-14.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28dd_Testimonial%20Standard%20Card-13.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/6458c625291a94a195e6cf3a/6458c625291a94090ae6cf95_Rectangle%2035.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28db_Testimonial%20Standard%20Card-11.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28d7_Testimonial%20Standard%20Card-16.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28d5_Testimonial%20Standard%20Card-10.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28cd_Testimonial%20Standard%20Card-7.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28c5_Testimonial%20Standard%20Card-6.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28cb_Testimonial%20Standard%20Card-2.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28c7_Testimonial%20Standard%20Card-3.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28c9_Testimonial%20Standard%20Card-4.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28d3_Testimonial%20Standard%20Card-1.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28d1_Testimonial%20Standard%20Card-5.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28d3_Testimonial%20Standard%20Card-1.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-              <img
-                src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28cf_Testimonial%20Standard%20Card-8.png"
-                alt=""
-                className="inline-block h-24 w-24 rounded-full object-cover opacity-40"
-              />
-            </div>
-            <div className="max-[991px]:max-w-[720px]">
-              <p className="mb-6 max-w-md text-white md:mb-10 lg:mb-12">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                aliquam, purus sit amet luctus venenatis, “Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit ut aliquam, purus sit amet
-                luctus venenatis, lectus magna fringilla urna, porttitor rhoncus
-                dolor purus non enim.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                aliquam, purus sit amet luctus venenatis, lectus magna fringilla
-                urna, porttitor rhoncus dolor purus non enim.”
-              </p>
-              <div className="flex gap-4">
-                <img
-                  src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f68133fc5cb4e29ed28e9_Group%2047904.svg"
-                  alt=""
-                  className="inline-block h-12"
-                />
-                <div>
-                  <p className="font-bold text-white">John Robert</p>
-                  <p className="text-sm text-white">Senior Webflow Developer</p>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="relative isolate  pb-32 pt-24 sm:pt-32">
+      <div
+        className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
+        aria-hidden="true"
+      >
+        <div
+          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] xl:ml-0 xl:mr-[calc(50%-12rem)]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-xl text-center">
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            We have worked with thousands of amazing people
+          </p>
         </div>
-      </section>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+          <figure className="rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
+            <blockquote className="p-6 text-lg font-semibold leading-7 tracking-tight text-gray-900 sm:p-12 sm:text-xl sm:leading-8">
+              <p>{`“${featuredTestimonial.body}”`}</p>
+            </blockquote>
+            <figcaption className="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-gray-900/10 px-6 py-4 sm:flex-nowrap">
+              <img
+                className="h-10 w-10 flex-none rounded-full bg-gray-50"
+                src={featuredTestimonial.author.imageUrl}
+                alt=""
+              />
+              <div className="flex-auto">
+                <div className="font-semibold">{featuredTestimonial.author.name}</div>
+                <div className="text-gray-600">{`${featuredTestimonial.author.handle}`}</div>
+              </div>
+            </figcaption>
+          </figure>
+          {testimonials.map((columnGroup, columnGroupIdx) => (
+            <div key={columnGroupIdx} className="space-y-8 xl:contents xl:space-y-0">
+              {columnGroup.map((column, columnIdx) => (
+                <div
+                  key={columnIdx}
+                  className={classNames(
+                    (columnGroupIdx === 0 && columnIdx === 0) ||
+                      (columnGroupIdx === testimonials.length - 1 && columnIdx === columnGroup.length - 1)
+                      ? 'xl:row-span-2'
+                      : 'xl:row-start-1',
+                    'space-y-8'
+                  )}
+                >
+                  {column.map((testimonial) => (
+                    <figure
+                      key={testimonial.author.handle}
+                      className="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-gray-900/5"
+                    >
+                      <blockquote className="text-gray-900">
+                        <p>{`“${testimonial.body}”`}</p>
+                      </blockquote>
+                      <figcaption className="mt-6 flex items-center gap-x-4">
+                        <img className="w-12  " src={testimonial.author.imageUrl} alt="" />
+                        <div>
+                          <div className="font-semibold">{testimonial.author.name}</div>
+                          <div className="text-gray-600">{`${testimonial.author.handle}`}</div>
+                        </div>
+                      </figcaption>
+                    </figure>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  );
-};
-
-export default Testimonials;
+  )
+}
