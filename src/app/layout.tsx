c426271a7b2d9c7c924739b2b7be7b1c795a6491
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-07-20 14:37:02 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import localFont from "next/font/local";
 import Script from 'next/script'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -39,19 +40,45 @@ export default function RootLayout({
     <html lang="en">
       <body className={satoshi.className+"dark"}>
         <div className='overflow-hidden relative'>
-        <noscript dangerouslySetInnerHTML={{ __html: `
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTW4XTSP"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>
-        `}} />
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NTW4XTSP');
-          `}
-        </Script>
+        <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NTW4XTSP');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H1XGRNBTDH"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H1XGRNBTDH');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
+      </Head>
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NTW4XTSP"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
         <Header/>
         {children}
         <a
