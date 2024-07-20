a29c2bee05cf43503637df2c7ba221faea70bb63
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-07-20 14:32:25 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import localFont from "next/font/local";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={satoshi.className+"dark"}>
         <div className='overflow-hidden relative'>
+        <noscript dangerouslySetInnerHTML={{ __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTW4XTSP"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `}} />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NTW4XTSP');
+          `}
+        </Script>
         <Header/>
         {children}
         <a
